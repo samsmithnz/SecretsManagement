944ece9309a6969faa61ae56cccfae900b4bf29e
Sam Smith <samsmithnz@gmail.com>
2020-04-16 11:15:56 -0400

    Added new project

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        string accessKey = "RLhncUwk+BkuBsSxCd8M+E1d/Y7llTTzQ/QC/C7Cdj3FUmE+msC9e5mF/VRI5wqWhPD+0QOXehW/DcGq9hBWSA==";
+    }
+}
