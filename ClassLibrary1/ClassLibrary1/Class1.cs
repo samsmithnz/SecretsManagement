c7e2e0d28f4a27a2dd0c690e6fb71e0cb07b3bd0
Sam Smith <samsmithnz@gmail.com>
2020-04-16 11:16:28 -0400

    REmoved the secret!

--- ClassLibrary1/ClassLibrary1/Class1.cs
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -4,6 +4,6 @@ namespace ClassLibrary1
 {
     public class Class1
     {
-        string accessKey = "RLhncUwk+BkuBsSxCd8M+E1d/Y7llTTzQ/QC/C7Cdj3FUmE+msC9e5mF/VRI5wqWhPD+0QOXehW/DcGq9hBWSA==";
+        string accessKey = "***";
     }
 }
