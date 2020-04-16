e78018131ab5bd0a83ad2931c29ef5e006c01e28
Sam Smith <samsmithnz@gmail.com>
2020-04-16 11:18:22 -0400

    added another secret

--- ClassLibrary1/ClassLibrary1/Class1.cs
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -5,5 +5,6 @@ namespace ClassLibrary1
     public class Class1
     {
         string accessKey = "***";
+        string connectionString = "DefaultEndpointsProtocol=https;AccountName=secretsmanagementstorage;AccountKey=gHnN2tkGKXzRsxH5YbYlg0MUYknEBjbGptCwl0b5rs+F5Ud5K4GRSKCcYldZU8MvyRd7CdlQo4lHUJLH4KhxtA==;EndpointSuffix=core.windows.net";
     }
 }
